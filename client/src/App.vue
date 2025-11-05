713e244938eb6975b7a0fdf368540e36d137881a
Josh <jsdev305@gmail.com>
2025-11-05 12:25:55 -0500

    update

--- client/src/App.vue
+++ client/src/App.vue
@@ -30,10 +30,10 @@ onMounted(async () => {
       <section v-if="hasMounted" class="hero flex flex-col md:flex-row items-center justify-center gap-6">
         <div class="w-full md:max-w-xl flex flex-col items-center justify-center gap-12">
           <h1 class="text-2xl text-glow md:text-[6rem] lg:text-[14rem] font-bold font-sci-fi uppercase z-10">
-            CostmoPort</h1>
+            CosmoPort</h1>
 
           <button @click="showHero = false"
-            class="border-2 p-2 cursor-pointer rounded-xl border-primary text-6xl md:w-fit w-full uppercase font-sci-fi">Explore
+            class="border-2 p-2 hover-bg hover:bg-primary hover:text-accent cursor-pointer rounded-xl border-primary text-6xl md:w-fit w-full uppercase font-sci-fi">Explore
             Voyages</button>
         </div>
 
