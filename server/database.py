62d183f77633be3724db2cbff79f8ab8570c3db2
jsdev305 <jsdev305@gmail.com>
2026-01-09 10:36:30 -0500

    updated env dir path logic, removed console log for the collection

--- server/database.py
+++ server/database.py
@@ -3,11 +3,13 @@ from pathlib import Path
 from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
+
 # Use absolute import so it works when running from project root
 from server.models.models import Voyage
+
 load_dotenv()
 
-#get the mongo uri
+# get the mongo uri
 BASE_DIR = Path(__file__).resolve().parent
 load_dotenv(BASE_DIR / ".env")
 
@@ -16,11 +18,10 @@ MONGO_URI = os.getenv("MONGODB_URI")
 # create the async client
 client = AsyncIOMotorClient(MONGO_URI)
 
+
 async def init_db():
-	await init_beanie(
-		database=client["cosmoport"],
-		document_models=[Voyage],
-	)
-	print("Connected to the MongoDB cluster ")
-	collections = await client["cosmoport"].list_collection_names()
-	print("Collections : ", collections)
+    await init_beanie(
+        database=client["cosmoport"],
+        document_models=[Voyage],
+    )
+    print("Connected to the MongoDB cluster ")
